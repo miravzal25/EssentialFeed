588dbe9ba867fa64aaa1eb5c64f5346c3dafabb5
Miravzal <39032972+miravzal25@users.noreply.github.com>
2025-01-02 19:55:03 +0500

    Add prototype feed data for demoing purposes

---
+++ Prototype/Prototype/FeedImageViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageViewModel.swift
+//  Prototype
+//
+//  Created by Miravzal Sultonov on 1/2/25.
+//
+
+import Foundation
+
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
