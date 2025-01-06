6fc73042bba7e529f3b541ef81f74c78824c8f4d
Miravzal <39032972+miravzal25@users.noreply.github.com>
2025-01-06 14:02:30 +0500

    Add `UIRefreshControl` to prototype to simulate async loading of the feed

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class FeedViewController: UITableViewController {
     
-    private let feed = FeedImageViewModel.prototypeFeed
+    private var feed: [FeedImageViewModel] = []
     
     override init(style: UITableView.Style) {
         super.init(style: style)
@@ -22,10 +22,20 @@ final class FeedViewController: UITableViewController {
         tableView.estimatedRowHeight = 580
         tableView.dataSource = self
         tableView.contentInset = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        
+        refresh()
+        tableView.setContentOffset(CGPoint(x: 0, y: -tableView.contentInset.top), animated: false)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,5 +47,16 @@ final class FeedViewController: UITableViewController {
         let model = feed[indexPath.row]
         cell.configure(with: model)
         return cell
+    }
+    
+    @objc private func refresh() {
+        refreshControl?.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            if self.feed.isEmpty {
+                self.feed = FeedImageViewModel.prototypeFeed
+                self.tableView.reloadData()
+            }
+            self.refreshControl?.endRefreshing()
+        }
     }
 }
