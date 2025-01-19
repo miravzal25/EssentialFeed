1cf153101f63854d0d77f3f758b7ffb8dc30f000
Miravzal <39032972+miravzal25@users.noreply.github.com>
2025-01-19 19:46:39 +0500

    Decouple `FeedRefreshViewController` from the concrete `FeedPresenter` dependency with a closure.

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,7 +13,7 @@ public final class FeedUIComposer {
 
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(presenter: feedPresenter)
+        let refreshController = FeedRefreshViewController(loadFeed: feedPresenter.loadFeed)
         let feedController = FeedViewController(refreshController: refreshController)
         feedPresenter.loaderView = WeakRefVirtualProxy(object: refreshController)
         feedPresenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 public final class FeedRefreshViewController: NSObject, FeedLoadingView {
-    private let presenter: FeedPresenter
+    private let loadFeed: () -> Void
 
     public lazy var view = loadView()
 
-    public init(presenter: FeedPresenter) {
-        self.presenter = presenter
+    public init(loadFeed: @escaping () -> Void) {
+        self.loadFeed = loadFeed
     }
     
     @objc public func refresh() {
-        presenter.loadFeed()
+        loadFeed()
     }
     
     private func loadView() -> UIRefreshControl {
